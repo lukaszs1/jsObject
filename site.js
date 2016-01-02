9b9fa75e5a990a8b1e2c6088aa13a32935b94cf3
lukas <lukas@lukas>
2016-01-02 00:54:21 +0000

    Objest inside the anonimous closure

---
+++ site.js
@@ -0,0 +1,20 @@
+
+
+var runLinks =( function  () {
+	var linkList = {
+	Google:"http://goolle.com",
+	Micosoft:"http://micosoft.com",
+	Apple:"http://apple.com"
+	};
+	var list= document.querySelectorAll('.nav');
+	var output = '<ul>';
+	for (var ke in linkList) {
+		output+= '<li><a href="'+linkList[ke]+'">'+ke+'</a></li>';
+		
+	};
+	output+='</ul>';
+	for (var i = list.length - 1; i >= 0; i--) {
+		list[i].innerHTML=output;
+	};
+
+})();
